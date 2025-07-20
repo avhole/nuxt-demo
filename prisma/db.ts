e6f404feb3f357b24b36db78ac59a84e6e35fa4d
hugh <hugh.he@everylin.ai>
2025-07-20 22:16:26 +0800

    debug:build

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,17 +1,19 @@
 // https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+// import { PrismaClient } from '@prisma/client'
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate())
-}
+	return new PrismaClient().$extends(withAccelerate());
+};
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
+	prisma: PrismaClientSingleton | undefined;
+};
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
